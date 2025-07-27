e5b5720b1ec7bd78770aeaef591c20c567eafe78
thanhdung96 <duduong96@outlook.com>
2025-07-27 22:15:59 +0700

    cleanup std.
    Modifications qui seront validées :
    	modifié :         main.cpp

--- main.cpp
+++ main.cpp
@@ -4,20 +4,24 @@
 #include <string>
 #include <thread>
 
+using std::string, std::cout, std::cin, std::endl;
+using std::getline, std::setw, std::setfill, std::flush;
+using std::this_thread::sleep_for, std::chrono::milliseconds;
+
 int main() {
     int hours = 0, minutes = 0, seconds = 0;
-    std::string message;
+    string message;
 
-    std::cout << "Enter hours: ";
-    std::cin >> hours;
-    std::cout << "Enter minutes: ";
-    std::cin >> minutes;
-    std::cout << "Enter seconds: ";
-    std::cin >> seconds;
-    std::cin.ignore();  // Clear newline from input buffer
+    cout << "Enter hours: ";
+    cin >> hours;
+    cout << "Enter minutes: ";
+    cin >> minutes;
+    cout << "Enter seconds: ";
+    cin >> seconds;
+    cin.ignore();  // Clear newline from input buffer
 
-    std::cout << "Enter message to display when time is up: ";
-    std::getline(std::cin, message);
+    cout << "Enter message to display when time is up: ";
+    getline(cin, message);
 
     if (hours < 0) hours = 0;
     if (minutes < 0) minutes = 0;
@@ -26,11 +30,11 @@ int main() {
     int total_seconds = hours * 3600 + minutes * 60 + seconds;
 
     if (total_seconds == 0) {
-        std::cout << "Timer set to 0 seconds. Exiting.\n";
+        cout << "Timer set to 0 seconds. Exiting.\n";
         return 0;
     }
 
-    std::cout << "\nTimer started for " << hours << "h " << minutes << "m "
+    cout << "\nTimer started for " << hours << "h " << minutes << "m "
               << seconds << "s.\n";
 
     while (total_seconds > 0) {
@@ -38,26 +42,26 @@ int main() {
         int m = (total_seconds % 3600) / 60;
         int s = total_seconds % 60;
 
-        std::cout << "\r" << std::setw(2) << std::setfill('0') << h << ":"
-                  << std::setw(2) << std::setfill('0') << m << ":"
-                  << std::setw(2) << std::setfill('0') << s << " remaining "
-                  << std::flush;
+        cout << "\r" << setw(2) << setfill('0') << h << ":"
+                  << setw(2) << setfill('0') << m << ":"
+                  << setw(2) << setfill('0') << s << " remaining "
+                  << flush;
 
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        sleep_for(std::chrono::seconds(1));
         --total_seconds;
     }
 
-    std::cout << "\r00:00:00 remaining " << std::endl;
+    cout << "\r00:00:00 remaining " << endl;
 
     // Display custom message
-    std::cout << message << std::endl;
+    cout << message << endl;
 
     // Ring the bell 3 times
     for (int i = 0; i < 3; ++i) {
-        std::cout << "\a";
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        cout << "\a";
+        sleep_for(milliseconds(500));
     }
-    std::cout << "Time's up!" << std::endl;
+    cout << "Time's up!" << endl;
 
     return 0;
 }
